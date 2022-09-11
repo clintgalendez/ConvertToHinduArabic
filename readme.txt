89cb4d498a7340a64780fd00100bb85d3bb782e6
clintgalendez <clintgalendez@gmail.com>
2022-09-11 14:35:11 +0800

    init commit

---
+++ readme.txt
@@ -0,0 +1,17 @@
+|----------------------------------------------|
+| Roman Numerals to Hindu-Arabic Number Format |
+|----------------------------------------------|
+
+Within the java file is a class called convertFormat
+with a method called toHinArab(). This method takes a Roman Numeral
+string as a parameter and returns the equivalent Hindu-Arabic number
+By checking each character in the string, the method determines the
+value of each character and adds it to the total. If the value of the
+current character is less than the value of the next character, the
+method subtracts the current value from the total. Otherwise, the
+method adds the current value to the total. The method returns the
+total.
+
+Created by: Clint Jonathan A. Galendez
+From: Xavier University - Ateneo de Cagayan - Philippines
+Bachelor of Science In Computer Science
---
+++ src/PE_Galendez.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+//Clint Jonathan A. Galendez
+//BSCS 2 - CSCC 20 A
+class convertFormat{
+    public int toHinArab(String romanNumeral) throws Exception {
+        int finalValue = 0;
+
+        for (int i = 0; i < romanNumeral.length(); i++){
+            switch (romanNumeral.charAt(i)) {
+                case 'I' -> finalValue += 1;
+                case 'V' -> finalValue += 5;
+                case 'X' -> finalValue += 10;
+                case 'L' -> finalValue += 50;
+                case 'C' -> finalValue += 100;
+                case 'D' -> finalValue += 500;
+                case 'M' -> finalValue += 1000;
+                default -> throw new Exception("Invalid Roman Numeral");
+            }
+
+            if (i > 0 && (romanNumeral.charAt(i) == 'V' || romanNumeral.charAt(i) == 'X') && romanNumeral.charAt(i - 1) == 'I') {
+                finalValue -= 2;
+            }
+        }
+        return finalValue;
+    }
+}
+
+public class PE_Galendez {
+    public static void main(String[] args){
+        convertFormat convert = new convertFormat();
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter a Roman Numeral: ");
+            String romanNumeral = input.nextLine().toUpperCase();
+            try {
+                System.out.println("Converted Number: " + convert.toHinArab(romanNumeral));
+                break;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
